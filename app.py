c30f111823e3cc917e91f74a9b07c82e958bb525
GreendoV <46889930+GreendoV@users.noreply.github.com>
2019-03-07 09:36:56 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route('/api')
+def getCode():
+	code = request.args.get('code')
+
+if __name__ == '__main__':
+	app.run(debug=True, port=5000)
