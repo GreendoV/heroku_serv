e3e00f1dd4caf31bbe3033d7b8f829e8febb49cc
GreendoV <46889930+GreendoV@users.noreply.github.com>
2019-03-07 10:04:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask,request
 
 app = Flask(__name__)
 
+@app.route('/')
+def welcome():
+	return "Welcome to my Server for DeezerAPI"
+
 @app.route('/api')
 def getCode():
 	code = request.args.get('code')
 	return code
 if __name__ == '__main__':
-	app.run(debug=True, port=5000)
+	app.run(debug=True, port=80)
